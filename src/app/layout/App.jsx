9caf42e60e6c53530a9aef0c98bc75275d62eb6a
Amoury <ansar.memon@hotmail.com>
2018-07-19 14:58:50 +0400

    Integrated a working form Component

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
-
+import React, { Component, Fragment } from 'react';
+import { Container } from 'semantic-ui-react';
+import EventDashboard from '../../features/event/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/navBar/NavBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <Fragment>
+        <NavBar/>
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </Fragment>
     );
   }
 }
