e2dde1b9bc7e8d8c7a318370e43e16f9a16b2d8e
Amoury <ansar.memon@hotmail.com>
2018-07-30 11:27:57 +0400

    Auth Integrated

--- src/features/nav/navBar/NavBar.jsx
+++ src/features/nav/navBar/NavBar.jsx
@@ -1,48 +1,73 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Menu, Container, Button } from "semantic-ui-react";
-import { NavLink, Link, withRouter } from 'react-router-dom';
-import SignedOutMenu from '../Menus/SignedOutMenu';
-import SignedInMenu from '../Menus/SignedInMenu';
+import { NavLink, Link, withRouter } from "react-router-dom";
+import SignedOutMenu from "../Menus/SignedOutMenu";
+import SignedInMenu from "../Menus/SignedInMenu";
+import { openModal } from '../../modals/modalActions';
+import { signOutUser } from '../../auth/authActions';
+
+const actions = {
+  openModal,
+  signOutUser
+}
 
 class NavBar extends Component {
-  state = {
-    authenticated: false
-  }
-
-  handleSignIn = () => {
-    this.setState({
-      authenticated: true
-    })
-  }
   
+  handleSignIn = () => {
+    this.props.openModal('LoginModal')
+  };
+  
+  handleRegister = () => {
+    this.props.openModal('RegisterModal')
+  };
+
   handleSignOut = () => {
-    this.setState({
-      authenticated: false
-    });
-    this.props.history.push('/');
-  }
+    this.props.signOutUser;
+    this.props.history.push("/");
+  };
 
   render() {
-    const {authenticated} = this.state;
+    const { auth } = this.props;
+    const authenticated = auth.authenticated;
 
-    return <Menu inverted fixed="top">
+    return (
+      <Menu inverted fixed="top">
         <Container>
           <Menu.Item as={Link} to="/" header>
             <img src="/assets/logo.png" alt="logo" />
             Re-vents
           </Menu.Item>
           <Menu.Item as={NavLink} to="/events" name="Events" />
-          { authenticated && 
-          <Menu.Item as={NavLink} to="/people" name="People" /> }
-          { authenticated && 
-          <Menu.Item>
-            <Button as={Link} to="/createEvent" floated="right" positive inverted content="Create Event" />
-          </Menu.Item> }
+          {authenticated && (
+            <Menu.Item as={NavLink} to="/people" name="People" />
+          )}
+          {authenticated && (
+            <Menu.Item>
+              <Button
+                as={Link}
+                to="/createEvent"
+                floated="right"
+                positive
+                inverted
+                content="Create Event"
+              />
+            </Menu.Item>
+          )}
 
-          {authenticated ? <SignedInMenu signOut={this.handleSignOut} /> : <SignedOutMenu signIn={this.handleSignIn} />}
+          {authenticated ? (
+            <SignedInMenu currentUser={auth.currentUser} signOut={this.handleSignOut} />
+          ) : (
+            <SignedOutMenu signIn={this.handleSignIn} register={this.handleRegister}/>
+          )}
         </Container>
-      </Menu>;
+      </Menu>
+    );
   }
 }
 
-export default withRouter(NavBar);
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+
+export default withRouter(connect(mapStateToProps, actions)(NavBar));
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,13 +3,16 @@ import { reducer as formReducer } from 'redux-form';
 import eventReducer from '../features/event/eventReducer';
 import testReducer from '../features/testarea/testReducer';
 import modalReducer from '../features/modals/modalReducer';
+import authReducer from '../features/auth/authReducer';
+
 
 
 const rootReducer = combineReducers({
   test: testReducer,
   form: formReducer,
   events: eventReducer,
-  modals: modalReducer
+  modals: modalReducer,
+  auth: authReducer
 })
 
 export default rootReducer;
